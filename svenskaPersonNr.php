8c5c6d14666ef7ac92a5f17103f14f32807f3bdb
simonlindstedt <70521702+simonlindstedt@users.noreply.github.com>
2020-11-03 15:12:51 +0100

    more stupid scripts

--- svenskaPersonNr.php
+++ svenskaPersonNr.php
@@ -2,9 +2,42 @@
 
 declare(strict_types=1);
 
-for ($i = 0; $i < 200; $i++) {
-  $year = rand(1920, 2020);
-  $month = rand(1, 12);
-  $day = rand(1, 29);
-  $last = rand(1000, 9999);
+for ($i = 0; $i < 1000; $i++) {
+
+  $year = (string)rand(1920, 2020);
+
+  $month = (string)rand(1, 12);
+
+  if (strlen($month) < 2) {
+    $month = "0" . $month;
+  }
+
+  $day = (string)rand(1, 29);
+
+  if (strlen($day) < 2) {
+    $day = "0" . $day;
+  }
+
+  $last = (string)rand(1000, 9999);
+
+  $numbers[] = $year . $month . $day . $last;
 }
+
+$uniquePersonalNumbers = array_unique($numbers);
+
+print_r($uniquePersonalNumbers);
+
+$file = fopen("person_nummer.sql", "w");
+
+$i = 0;
+
+foreach ($uniquePersonalNumbers as $number) {
+
+  $i++;
+
+  $sql = "INSERT INTO passport_database (social_security_number) VALUES($number) WHERE id = $i;\n";
+
+  fwrite($file, $sql);
+}
+
+fclose($file);
